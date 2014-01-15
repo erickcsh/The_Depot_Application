5ef38013cf5e3ed7d91b5ff6079c310677188d4b
Jose Alberto <josealberto.ch.b@gmail.com>
2014-01-15 09:00:49 -0600

    Added the time where the user enter on the website

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :set_date
+  def set_date
+    time = Time.now
+    @date_loaded = time.strftime("%Y-%m-%d %H:%M:%S")
+  end
 end
