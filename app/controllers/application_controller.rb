1aa3fcebc8c3d5a0501ce7c0704a253a007434ac
Jose Alberto <josealberto.ch.b@gmail.com>
2014-01-15 11:15:02 -0600

    Stored ID of the cart in the application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,20 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_filter :set_date
+
   def set_date
     time = Time.now
     @date_loaded = time.strftime("%Y-%m-%d %H:%M:%S")
   end
+
+  private
+
+  def current_cart
+    Cart.find(session[:cart_id])
+  rescue ActiveRecord::RecordNotFound
+    cart = Cart.create
+    session[:cart_id] = cart.id
+    cart
+  end
+
 end
