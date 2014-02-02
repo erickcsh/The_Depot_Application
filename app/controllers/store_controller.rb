694bc1482312a587ea33c52492407fc3665e77ca
Jose Alberto <josealberto.ch.b@gmail.com>
2014-02-01 18:26:23 -0600

    Added a new variable to the session to record how many times the user has accessed

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,8 @@
 class StoreController < ApplicationController
   def index
     @products = Product.all
+    @counter = session[:counter]
+    @counter.nil? ? @counter = 1 : @counter+=1
+    session[:counter] = @counter
   end
 end
